c4b659685271693d32223028e1f142f25543e4ca
Hannes Preishuber <hannesp@ppedv.de>
2023-10-12 16:44:42 +0200

    REST Todo Service mit Filter

---
+++ BlazorTrainingOkt_237423/Pages/modul4/ToDoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorTrainingOkt_237423.Pages.modul4
+{
+    public class ToDoModel
+    {
+ 
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
