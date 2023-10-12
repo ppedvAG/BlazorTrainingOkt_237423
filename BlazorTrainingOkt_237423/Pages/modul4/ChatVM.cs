7e93e30c6a48f24c5df441e30f19b30b5c1fdca3
Hannes Preishuber <hannesp@ppedv.de>
2023-10-12 16:22:14 +0200

    minichat mit VM und Action Callback

---
+++ BlazorTrainingOkt_237423/Pages/modul4/ChatVM.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorTrainingOkt_237423.Pages.modul4
+{
+    public class ChatVM
+    {
+        public Action OnAdd;
+        public List<Nachricht> ChatListe { get; set; } = new List<Nachricht>();
+        public void Add(string msg)
+        {
+            ChatListe.Add(new Nachricht() { Text = msg });
+            OnAdd?.Invoke();
+
+        }
+    }
+}
---
+++ BlazorTrainingOkt_237423/Pages/modul4/Nachricht.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorTrainingOkt_237423.Pages.modul4
+{
+    public class Nachricht
+    {
+        public string Text { get; set; }
+    }
+}
