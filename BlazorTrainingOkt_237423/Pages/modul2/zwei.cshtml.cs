905191ff790d228b4d338656a4aaa7ae2721cd12
Hannes Preishuber <hannesp@ppedv.de>
2023-10-12 11:50:25 +0200

    Navigationmanager

---
+++ BlazorTrainingOkt_237423/Pages/modul2/zwei.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingOkt_237423.Pages.modul2
+{
+    public class zweiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
