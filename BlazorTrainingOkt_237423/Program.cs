3915f4459c9e09172abbc8e48bbff6526ab6c71a
Hannes Preishuber <hannesp@ppedv.de>
2023-10-13 09:47:53 +0200

    Entity Framework Kunden Liste, Filtern, Auswahl

--- BlazorTrainingOkt_237423/Program.cs
+++ BlazorTrainingOkt_237423/Program.cs
@@ -1,7 +1,9 @@
 using BlazorTrainingOkt_237423.Data;
+using BlazorTrainingOkt_237423.Models;
 using BlazorTrainingOkt_237423.Pages.modul4;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<ChatVM>();
 builder.Services.AddHttpClient();
+builder.Services.AddDbContext<NorthwindContext>(o=>o.UseSqlServer(builder.Configuration.GetConnectionString("Northwind")));
 
 var app = builder.Build();
 
