00bfd8c46d4ac4d763714b73dbabf20efaaadf0e
Hannes Preishuber <hannesp@ppedv.de>
2023-10-13 15:57:54 +0200

    JS warmup

---
+++ BlazorTrainingOkt_237423/Pages/modul8/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingOkt_237423.Pages.modul8
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
