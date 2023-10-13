480ca79129f5ecb57a57f4f3b3f38b554e7040ce
Hannes Preishuber <hannesp@ppedv.de>
2023-10-13 09:58:22 +0200

    HTML Formular Basics (TODO FIX)

---
+++ BlazorTrainingOkt_237423/Pages/modul5/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingOkt_237423.Pages.modul5
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+        }
+    }
+}
