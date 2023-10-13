6cb7c1a0fad900cd0b522779dc0c3adac5f0cc5f
Hannes Preishuber <hannesp@ppedv.de>
2023-10-13 10:05:58 +0200

    PoCo mit DataAnnotations für Validierung

---
+++ BlazorTrainingOkt_237423/Pages/modul5/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTrainingOkt_237423.Pages.modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Da muss was rein")]
+        [StringLength(10,MinimumLength =2,ErrorMessage ="Muss zwischen 2 und 10 lang sein")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        [Range(150,190,ErrorMessage ="Keine Achterbahn für Dich")]
+        public int  Gross { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        public DateTime GebDat { get; set; }
+    }
+}
